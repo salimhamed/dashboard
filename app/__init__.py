91dfe004a221700ca93e674f01761c4c02a0f1f3
Salim Hamed <salimhamed@gmail.com>
2015-04-26 12:19:16 -0700

    added comments and docstrings

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
 from config import config
 
-# flask extensions
+# initialize flask extensions (note, initalized with no Flask app instance)
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
@@ -13,10 +13,18 @@ db = SQLAlchemy()
 
 
 def create_app(config_name):
+    """
+    Flask Application Factory that takes configuration settings and returns
+    a Flask application.
+    """
+    # initalize instance of Flask application
     app = Flask(__name__)
+
+    # import configuration settings into Flask application instance
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
+    # initialize Flask extensions
     bootstrap.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
