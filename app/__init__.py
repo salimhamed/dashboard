0fa119418cded737e23188033d1a9e65701e86eb
Salim Hamed <salimhamed@gmail.com>
2015-04-26 12:51:27 -0700

    added comments and docstrings

--- app/__init__.py
+++ app/__init__.py
@@ -30,6 +30,7 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
 
+    # register blueprint with Flask application
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,8 +1,13 @@
 from flask import Blueprint
 
+# Create a Flask Blueprint with will define views and errors
 main = Blueprint(
     'main',  # blueprint name
     __name__  # module where blueprint is located
 )
 
+# This Blueprint is defined as a Package named 'main'.  The modules imported
+# below are part of the Blueprint.  The views and erros modules must be
+# imported after the Blueprint object is instantiated because they rely on the
+# Blueprint object to define routes and error handles
 from . import views, errors
