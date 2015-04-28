72ea4f429d33ebf159457375b658dd92d81ed269
Salim Hamed <salimhamed@gmail.com>
2015-04-27 20:39:36 -0700

    Replaced exisitng front-end with SB Admin front-end

--- app/main/views.py
+++ app/main/views.py
@@ -1,16 +1,14 @@
 from flask import render_template, session, redirect, url_for, current_app
+from flask.ext.login import current_user
 from .. import db
 from ..models import User
 from ..email import send_email
 from . import main
-from .forms import SearchForm
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    search_form = SearchForm()
-    if search_form.validate_on_submit():
-        session['company'] = search_form.company.data
-        return redirect(url_for('main.index'))
-    return render_template('index.html', form=search_form,
-                           company=session.get('company'))
+    if current_user.is_anonymous():
+        return render_template('welcome.html')
+    else:
+        return render_template('index.html')
