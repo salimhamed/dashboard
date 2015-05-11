c1f30c8157004ecb7f89f7f8807a6d84c5fff36c
Salim Hamed <salimhamed@gmail.com>
2015-05-11 08:43:10 -0700

    finished v1 of typeahead search functionality.

--- app/static/js/autocomplete.js
+++ app/static/js/autocomplete.js
@@ -1,28 +1,45 @@
-// define array of data
-var local_data = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-];
+$(document).ready(function() {
 
-// construct the suggestion engine
-var engine = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.whitespace,
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  local: local_data
-});
+    // define web url
+    var search_url = 'http://' + $SCRIPT_ROOT
+    var prefetch_url = 'http://' + $SCRIPT_ROOT
 
-$('#bloodhound .typeahead').typeahead({
-  hint: true,
-  highlight: true,
-  minLength: 1
-},
-{
-  name: 'search',
-  source: engine
+    //Set up "Bloodhound" Options
+    var suggestion_class = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('vval'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+
+        prefetch: {
+            url: search_url + '/_search_prefetch',
+            filter: function(x) {
+                return $.map(x.results, function(item) {
+                    return {vval: item.name};
+                });
+            }
+        },
+
+        remote: {
+             url: search_url + '/_search?query=%QUERY',
+             filter: function(x) {
+                 return $.map(x.results, function(item) {
+                     return {vval: item.name};
+                 });
+             },
+             wildcard: "%QUERY",
+         }
+    });
+
+    // Initialize Typeahead with Parameters
+    $('#bloodhound .typeahead').typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 1
+    },
+    {
+        name: 'vval',
+        displayKey: 'vval',
+        limit: 10,
+        source: suggestion_class
+    });
+
 });
