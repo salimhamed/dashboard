0a68a294a5b8839c65721c763929d2b17693175c
Salim Hamed <salimhamed@gmail.com>
2015-06-07 12:21:56 -0700

    moved all search views to seperate package.

--- app/search/views.py
+++ app/search/views.py
@@ -1,22 +1,39 @@
-from flask import jsonify, Response, render_template, request
+from flask import Response, render_template, request
 from flask_login import login_required, current_user
 from . import search
 from .forms import SearchForm
-from ..models import Firm, Company
+from ..models import Firm, Company, FirmType, FirmTier
 import json
 
 
 @search.route('/', methods=['GET', 'POST'])
 @login_required
 def search_app():
-    query = request.args.get('query')
-    firms = Firm.query.with_entities(Firm.name)\
+    query = request.form['query'].lower()
+
+    # query for results
+    firms = Firm.query.join(FirmType).join(FirmTier)\
+        .with_entities(Firm, FirmType, FirmTier)\
         .filter(Firm.name.ilike('{}%'.format(query)))\
         .order_by(Firm.name.asc()).all()
-    companies = Company.query.with_entities(Company.name)\
+    companies = Company.query\
         .filter(Company.name.ilike('{}%'.format(query)))\
         .order_by(Company.name.asc()).all()
-    return render_template('search/results.html', firms=firms,
+
+    # parse firms
+    vc_firms = []
+    ai_firms = []
+    su_firms = []
+    for firm in firms:
+        if firm.FirmType.firm_type_code == 'vc':
+            vc_firms.append(firm)
+        if firm.FirmType.firm_type_code == 'ai':
+            ai_firms.append(firm)
+        if firm.FirmType.firm_type_code == 'su':
+            su_firms.append(firm)
+
+    return render_template('search/results.html', vc_firms=vc_firms,
+                           ai_firms=ai_firms, su_firms=su_firms,
                            companies=companies)
 
 
