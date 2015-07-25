1b37a8fb442df2ac0aaf932191b4a1452cb3faab
Salim Hamed <salimhamed@gmail.com>
2015-07-25 12:03:01 -0700

    Added class method to production config for sending logging errors via email.

--- config.py
+++ config.py
@@ -53,6 +53,29 @@ class ProductionConfig(Config):
         "postgresql+psycopg2://" + os.environ.get("PG_ADMIN", 'admin') + ":" \
         + os.environ.get("PG_PASSWORD", 'password') + "@localhost/dashboard"
 
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+        # email errors to the administrators
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = None
+        secure = None
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MAIL_USE_TLS', None):
+                secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr=cls.MAIL_SENDER,
+            toaddrs=[cls.DASHBOARD_ADMIN],
+            subject=cls.MAIL_SUBJECT_PREFIX + ' Application Error',
+            credentials=credentials,
+            secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
 
 config = {
     'development_sqlite': DevelopmentConfigSqlite,
