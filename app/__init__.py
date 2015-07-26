523ee6ba4cc6d8395b8af2284b1c09bb31ad2a8f
Salim Hamed <salimhamed@gmail.com>
2015-07-25 18:28:58 -0700

    added SSL rerouting

--- app/__init__.py
+++ app/__init__.py
@@ -38,6 +38,11 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
 
+    # redirect all http requests to https
+    if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
+        from flask.ext.sslify import SSLify
+        sslify = SSLify(app)
+
     # register 'main' blueprint with Flask application
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
